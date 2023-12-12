17f4213f76309603bde7e2b2f925c2dbf24b5d44
Yaweihsu0201 <146183597+Yaweihsu0201@users.noreply.github.com>
2023-12-13 00:15:36 +0800

    Update Foo.cpp

--- Foo.cpp
+++ Foo.cpp
@@ -2,14 +2,13 @@
 #include <SDL_image.h>
 #include <stdio.h>
 #include <string>
-
 #include "Foo.h"
 
 Foo::Foo()
 {
     //Initialize the offsets
     mPosX = SCREEN_WIDTH - 100;
-    mPosY = 220;
+    mPosY = 400;
 	
 	mCollider.w = FOO_WIDTH;
 	mCollider.h = FOO_HEIGHT;
@@ -28,6 +27,7 @@ void Foo::handleEvent( SDL_Event& e )
         {
             case SDLK_LEFT: mVelX -= FOO_VEL; break;
             case SDLK_RIGHT: mVelX += FOO_VEL; break;
+            case SDLK_UP: mVelY += 5; break;
         }
     }
     //If a key was released
@@ -38,23 +38,25 @@ void Foo::handleEvent( SDL_Event& e )
         {
             case SDLK_LEFT: mVelX += FOO_VEL; break;
             case SDLK_RIGHT: mVelX -= FOO_VEL; break;
+            case SDLK_UP: mVelY -= 5; break;
         }
     }
 }
-
-void Foo::move(SDL_Rect& wall)
+void Foo::move()
 {
     //Move the dot left or right
     mPosX += mVelX;
+    mPosY -= mVelY;
 	mCollider.x = mPosX;
 	mCollider.y = mPosY;
 
     //If the dot went too far to the left or right
     
-    if( ( mPosX < 0 ) || ( mPosX + FOO_WIDTH > SCREEN_WIDTH ) || SDL_HasIntersection(&mCollider, &wall ) )
+    if( ( mPosX < 0 ) || ( mPosX + FOO_WIDTH > SCREEN_WIDTH ) )
     {
         //Move back
         mPosX -= mVelX;
+        mPosY += mVelY;
         mCollider.x = mPosX;
 		mCollider.y = mPosY;
     }
@@ -66,3 +68,4 @@ void Foo::render()
     //Show the dot
 	gFooTexture.render( mPosX, mPosY );
 }
+
