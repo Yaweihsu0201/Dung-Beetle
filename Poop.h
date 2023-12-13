8ca33cf49259cb8dc97f1a4247a491b8400c6d66
Yaweihsu0201 <146183597+Yaweihsu0201@users.noreply.github.com>
2023-12-13 15:15:36 +0800

    Create Poop.h

---
+++ Poop.h
@@ -0,0 +1,37 @@
+#ifndef POOP_H
+#define POOP_H
+
+#include "LTexture.h"
+
+extern LTexture gPoop;
+extern const int SCREEN_WIDTH;
+extern const int SCREEN_HEIGHT;
+
+class Poop
+{
+    public:
+		static const int POOP_WIDTH = 20;
+		static const int POOP_HEIGHT = 20;
+
+		//Maximum axis velocity of the dot
+		static const int POOP_VEL = 1;
+
+		//Initializes the variables
+		Poop();
+
+		//Moves the dot
+		void move();
+
+		//Shows the dot on the screen
+		void render();
+		
+
+    private:
+		//The X and Y offsets of the dot
+		int mPosX, mPosY;
+
+		//The velocity of the dot
+		int mVelX, mVelY;
+		SDL_Rect mCollider;
+};
+#endif
