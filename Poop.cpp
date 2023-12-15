49ea94409ff7caed937c53268046d95cc098e778
Yaweihsu0201 <146183597+Yaweihsu0201@users.noreply.github.com>
2023-12-15 23:41:52 +0800

    poop can now drop from the sky

--- Poop.cpp
+++ Poop.cpp
@@ -3,50 +3,242 @@
 #include <stdio.h>
 #include <string>
 #include "Poop.h"
-#define g 1;
-Poop::Poop()
+#include "Bug2.h"
+#include "Foo.h"
+#include "LTexture.h"
+//Screen dimension constants
+extern const int SCREEN_WIDTH;
+extern const int SCREEN_HEIGHT;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//The application time based timer
+class LTimer
 {
-    //Initialize the offsets
-    mPosX = SCREEN_WIDTH/2;
-    mPosY = 100;
-	
-	mCollider.w = POOP_WIDTH;
-	mCollider.h = POOP_HEIGHT;
-    //Initialize the velocity
-    mVelX = 0;
-    mVelY = 0;
+    public:
+		//Initializes variables
+		LTimer();
+
+		//The various clock actions
+		void start();
+		void stop();
+		void pause();
+		void unpause();
+
+		//Gets the timer's time
+		Uint32 getTicks();
+
+		//Checks the status of the timer
+		bool isStarted();
+		bool isPaused();
+
+    private:
+		//The clock time when the timer started
+		Uint32 mStartTicks;
+
+		//The ticks stored when the timer was paused
+		Uint32 mPausedTicks;
+
+		//The timer status
+		bool mPaused;
+		bool mStarted;
+};
+
+
+
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+//Scene textures
+LTexture gFooTexture;
+LTexture gBackgroundTexture;
+LTexture gPoop;
+LTexture gBug2;
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		{
+			printf( "Warning: Linear texture filtering not enabled!" );
+		}
+
+		//Create window
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			//Create renderer for window
+			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+			if( gRenderer == NULL )
+			{
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
 }
 
-void Poop::move()
+bool loadMedia()
 {
-    //Move the dot left or right
-    mPosX += mVelX;
-    mPosY -= mVelY-g;
-	mCollider.x = mPosX;
-	mCollider.y = mPosY;
+	//Loading success flag
+	bool success = true;
 
-    //If the dot went too far to the left or right
-    
-    if( ( mPosX < 0 ) || ( mPosX + POOP_WIDTH > SCREEN_WIDTH ) )
-    {
-        //Move back
-        mPosX -= mVelX;
-    } 
-    
-    if ( mPosY > 400) {
-    	mPosY += mVelY-1;
-	} else {
-		printf("in the air\n");
+	//Load Foo' texture
+	if( !gFooTexture.loadFromFile( "img/bug2.png" ) )
+	{
+		printf( "Failed to load Foo' texture image!\n" );
+		success = false;
+	}
+	//Load pipe
+	if( !gPoop.loadFromFile( "img/poop.png" ) )
+	{
+		printf( "Failed to load Foo' texture image!\n" );
+		success = false;
 	}
 	
+	//Load background texture
+	if( !gBackgroundTexture.loadFromFile( "img/background.png" ) )
+	{
+		printf( "Failed to load background texture image!\n" );
+		success = false;
+	}
 	
-	mCollider.x = mPosX;
-	mCollider.y = mPosY;
+	if( !gBug2.loadFromFile( "img/bug3.png" ) )
+	{
+		printf( "Failed to load Foo' texture image!\n" );
+		success = false;
+	}
+
+	return success;
 }
 
-void Poop::render()
+void close()
 {
-    //Show the dot
-	gPoop.render( mPosX, mPosY );
+	//Free loaded images
+	gFooTexture.free();
+	gBackgroundTexture.free();
+	gBug2.free();
+	gPoop.free();
+
+	//Destroy window	
+	SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
 }
 
+int main( int argc, char* args[] )
+{	
+	//Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{	
+		printf("Success to initialize!\n");
+		//Load media
+		if( !loadMedia() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{	
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+			
+			Foo foo;
+			Bug2 bug2;
+			Poop poop;
+			//While application is running
+			while( !quit )
+			{	
+				//Handle events on queue
+				while( SDL_PollEvent( &e ) != 0 )
+				{
+					//User requests quit
+					if( e.type == SDL_QUIT )
+					{
+						quit = true;
+					}
+					foo.handleEvent(e);
+					bug2.handleEvent(e);
+					
+				}
+				foo.move();
+				bug2.move();
+				poop.move();
+				//Clear screen
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+				SDL_RenderClear( gRenderer );
+
+				//Render background texture to screen
+				
+				gBackgroundTexture.render( 0, 0 ); 
+				//Render Foo' to the screen
+				foo.render();
+				bug2.render();
+				poop.render();
+				//Update screen
+				SDL_RenderPresent( gRenderer );
+				SDL_Delay(2);
+			}
+		}
+	}
+	
+	//Free resources and close SDL
+	close();
+
+	return 0;
+}
