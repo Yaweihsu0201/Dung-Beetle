c4f56460b2808e34c6e195efa43faf430221cb50
Yaweihsu0201 <146183597+Yaweihsu0201@users.noreply.github.com>
2023-12-16 02:19:18 +0800

    Update Poop.cpp

--- Poop.cpp
+++ Poop.cpp
@@ -6,27 +6,23 @@
 #define g 1;
 Poop::Poop()
 {
-    //Initialize the offsets
     mPosX = SCREEN_WIDTH/2;
     mPosY = 100;
 	
 	mCollider.w = POOP_WIDTH;
 	mCollider.h = POOP_HEIGHT;
-    //Initialize the velocity
     mVelX = 0;
     mVelY = 0;
 }
 
 void Poop::move()
 {
-    //Move the dot left or right
     mPosX += mVelX;
     mPosY += mVelY+g;
-    //If the dot went too far to the left or right
     
     if( ( mPosX < 0 ) || ( mPosX + POOP_WIDTH > SCREEN_WIDTH ) )
     {
-        //Move back
+        
         mPosX -= mVelX;
     } 
     
@@ -39,13 +35,21 @@ void Poop::move()
 	mCollider.x = mPosX;
 	mCollider.y = mPosY;
 }
-
+//! 
 void Poop::render()
 {
-    //Show the dot
-	gPoop.render( mPosX, mPosY );
+    
+	if (!withBug) {
+		gPoop.render( mPosX, mPosY );
+	}
 }
 
 SDL_Rect Poop::PoopCollider() {
 	return mCollider;
+}
+//! 
+void Poop::discard() {
+	mPosX = 300;
+	mPosY = 460;
+	withBug = true;
 }
