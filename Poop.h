a29079c70796d2e32a711e2522a4cf87926e8f1e
Yaweihsu0201 <146183597+Yaweihsu0201@users.noreply.github.com>
2023-12-16 02:19:45 +0800

    Update Poop.h

--- Poop.h
+++ Poop.h
@@ -8,31 +8,29 @@ extern const int SCREEN_WIDTH;
 extern const int SCREEN_HEIGHT;
 
 class Poop
-{
+{	
     public:
 		static const int POOP_WIDTH = 20;
 		static const int POOP_HEIGHT = 20;
 
-		//Maximum axis velocity of the dot
 		static const int POOP_VEL = 1;
 
-		//Initializes the variables
 		Poop();
 
-		//Moves the dot
 		void move();
 
-		//Shows the dot on the screen
 		void render();
 		
 		SDL_Rect PoopCollider();
+		//!
+		void discard();
 		
-
+		
+	
     private:
-		//The X and Y offsets of the dot
 		int mPosX, mPosY;
-
-		//The velocity of the dot
+		//!
+		bool withBug;
 		int mVelX, mVelY;
 		SDL_Rect mCollider;
 };
